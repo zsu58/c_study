2eeeea8073ca82acd7a0c3db7d7a9df09111a49d
Jisu Park <zsu58@icloud.com>
2022-09-26 22:45:47 +0900

    2022-09-26_1

---
+++ c4/c4_1_StdioChar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void main()
+{
+  char ch;
+  /*
+  getchar()을 통해 표준 입력 장치(stdin)인 키보드로부터 입력 받음. 
+  이 때 한 행의 문자열을 입력받음(한 글자가 아니라) 
+  이후 입력받은 문자열에서 첫 번째를 글자를 가져와 ch에 저장
+  */
+  ch = getchar(); 
+  putchar(ch); // 표준 입력 장치의 파일 버퍼에서 한 글자를 가져와 출력
+  ch = getchar(); // 표준 입력 장치의 파일 버퍼에서 한 글자를 가져와 ch 변수에 저장
+  putchar(ch);
+  ch = getchar();
+  putchar(ch);
+  printf("\n");
+}
+
+/*
+int getchar(void);
+표준 입력 장치(stdin)로부터 문자 한 글자를 읽어 와서 반환
+만일 표준 입력 장치에서 버퍼 (메모리)가 비어 있다면 사용자로부터 입력을 받아 반환하고 그렇지 않으면 버퍼에서 가져와서 반 환
+
+int putchar(int c);
+표준 출력 장치(stdout)로 한 글자를 출력한다.
+*/
---
+++ c4/c4_2_stdloString.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main()
+{
+  
+}
+
+/*
+char *gets(char *buffer);
+buffer : 입력받은 문자열을 저장할 메모리의 주소
+정상적이면 인자로 전달받은 메모리의 주소, 오류가 발생하면 NULL을 반환
+표준 입력 장치(stdin)에서 문자열을 입력받는 함수
+보안에 취약해 gets_s()를 사용하는 것이 더 좋음
+
+int puts(const char *string);
+string : 출력할 문자열이 저장된 메모리의 주소
+정상적이면 음수가 아닌 값을 반환, 오류가 발생하면 EOF를 반환
+표준 출력 장치(stdout)에 문자열을 출력하는 함수
+cf. 문자열이 출력된 이후 자동으로 개행 문자를 출력
+*/
+
+
